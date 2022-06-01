40439978cf1951d9c979e4dc93124d51e3593835
AlexJediVoin <studying.info.tech@gmail.com>
2022-06-01 15:56:21 +0500

    todolist task 9.

--- src/Store/todolists-tasks-reducer.test.ts
+++ src/Store/todolists-tasks-reducer.test.ts
@@ -19,3 +19,26 @@ test('ids should be equals', () => {
     expect(idFromTasks).toBe(action.payload.id);
     expect(idFromTodolists).toBe(action.payload.id);
 });
+
+test('property with todolistId should be deleted', () => {
+    const startState: TaskStateType = {
+        "todolistId1": [
+            { id: "1", title: "CSS", isDone: false },
+            { id: "2", title: "JS", isDone: true },
+            { id: "3", title: "React", isDone: false }
+        ],
+        "todolistId2": [
+            { id: "1", title: "bread", isDone: false },
+            { id: "2", title: "milk", isDone: true },
+            { id: "3", title: "tea", isDone: false }
+        ]
+    };
+
+    const action = RemoveTodolistAC("todolistId2");
+    const endState = tasksReducer(startState, action)
+
+    const keys = Object.keys(endState);
+
+    expect(keys.length).toBe(1);
+    expect(endState["todolistId2"]).toBeUndefined();
+});
